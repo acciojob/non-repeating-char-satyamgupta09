15bb67c540ec092ce9232ee411b760d533e39426
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2024-12-28 03:25:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length;i++){
+		if(i-1>=0){
+		   const s1=str.substring(0,i);
+		}
+		const s2=str.substring(i+1,str.length);
+		// prompt(s1,'   ',s2);
+		if(!s1.includes(str[i]) && !s2.includes(str[i])){
+			return str[i];
+		}
+	}
+	return null; 
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
