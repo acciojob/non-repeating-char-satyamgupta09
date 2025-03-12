66dd0c1656c04d21a765eeacb6fabd4f095e2d9d
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-03-13 02:02:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	for(let i=0;i<str.length;i++){
-		// if(i-1>=0){
-		   const s1=str.substring(0,i);
-		// }
-		const s2=str.substring(i+1,str.length);
-		// prompt(s1,'   ',s2);
-		if(!s1.includes(str[i]) && !s2.includes(str[i])){
-			return str[i];
+	// for(let i=0;i<str.length;i++){
+	// 	// if(i-1>=0){
+	// 	   const s1=str.substring(0,i);
+	// 	// }
+	// 	const s2=str.substring(i+1,str.length);
+	// 	// prompt(s1,'   ',s2);
+	// 	if(!s1.includes(str[i]) && !s2.includes(str[i])){
+	// 		return str[i];
+	// 	}
+	// }
+	let mp = new Map();
+	for(let x of str){
+		if(mp.has(x)){
+			mp.set(x,mp.get()+1);
+		}
+		else{
+			mp.set(x,1);
+		}
+	}
+
+	for(let [key, value] of mp){
+		if(value==1){
+			return key;
 		}
 	}
 	return null; 
 }
-const input = prompt("Enter a string");  
+// const input = prompt("Enter a string");  
 alert(firstNonRepeatedChar(input)); 
